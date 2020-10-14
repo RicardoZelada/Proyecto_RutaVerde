e263dd58d33397daffadbb65ad69da75d985a882
ricardo <ricky.zelada.a@gmail.com>
2020-10-13 23:37:58 -0300

    Finalizacion del proyecto
    Modificacion de Servicios.js y Servicios.html

--- Js/Servicios.js
+++ Js/Servicios.js
@@ -29,51 +29,34 @@ window.mostrarErrores = (erroresLista)=>{//metodo para motrar los errores desde 
 };
 
 window.cargarIngreso = ()=>{
-    let listaServicios = document.querySelector("#divActividades");
-    listaServicios.innerHTML = "";
+    //let listaServicios = document.querySelector(".card-body");
+    //listaServicios.innerHTML = "";
+        
+    let cardCliente = document.querySelector("#cardRegistro");
+    cardCliente.classList.add("row","row-cols-1","row-cols-md-2", "row-cols-lg-3");
+    cardCliente.innerHTML = "";
+    
     let imagen = "";
 
     window.ingreso.forEach((elementos)=>{
         if(elementos.opcion === "1"){
+            //creo el card
+            let card = document.createElement("div");
+            card.innerHTML = "";
+            card.classList.add("card","mt-5");
+            //creo el card header 
+            let cardHeader = document.createElement("div");
+            cardHeader.classList.add("card-header","bg-dark","text-white","text-center");
+            cardHeader.innerText = "Bienvenido a Ruta Verde " + elementos.nombre;
+            //creo el card body
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
             let uLista = document.createElement('ul');
             uLista.classList.add("list-group", "list-group-flush")
             uLista.innerHTML = "";
-            //creacion del Li para el nombre
             let liNombre = document.createElement("li");
             liNombre.classList.add("list-group-item");
-            liNombre.innerText = elementos.nombre + " usted escogio la actividad Trekking al Parque Nacional la Campana";
-            //creacion del Li para el telefono
-            let liFono = document.createElement("li");
-            liFono.classList.add("list-group-item");
-            liFono.innerText = "Su telefono es : " + elementos.telefono
-            //creacion del Li para el correo
-            let liMail = document.createElement("li");
-            liMail.classList.add("list-group-item");
-            liMail.innerText = "Corre electonico : " + elementos.email + ", enviaremos mas informacion sobre la actividad.";
-            //creacion del Li para el mensaje
-            let liMensaje = document.createElement("li");
-            liMensaje.classList.add("list-group-item");
-            liMensaje.innerText = elementos.mensaje;
-            //creacion del Li para la foto
-            liImagen = document.createElement("li");
-            liImagen.classList.add("list-group-item","p-5");
-            imagen = 'Lacampana.jpg';
-
-                uLista.appendChild(liNombre);
-                uLista.appendChild(liFono);
-                uLista.appendChild(liMail);
-                uLista.appendChild(liMensaje);
-                uLista.appendChild(liImagen);
-                listaServicios.appendChild(uLista);
-
-        }if(elementos.opcion === "2"){
-            let uLista = document.createElement('ul');
-            uLista.classList.add("list-group", "list-group-flush")
-            uLista.innerHTML = "";
-             //creacion del Li para el nombre
-            let liNombre = document.createElement("li");
-            liNombre.classList.add("list-group-item");
-            liNombre.innerText = elementos.nombre + " usted escogio la actividad Caminatas Familiares";
+            liNombre.innerText = elementos.nombre + " usted escogio la actividad Trekking al Parque la Campana";
             //creacion del Li para el telefono
             let liFono = document.createElement("li");
             liFono.classList.add("list-group-item");
@@ -88,21 +71,80 @@ window.cargarIngreso = ()=>{
             liMensaje.innerText = "Mensaje :" + elementos.mensaje;
             //creacion del Li para la foto
             liImagen = document.createElement("li");
-            liImagen.classList.add("list-group-item","p-5");
+            liImagen.classList.add("list-group-item","p-2");
             imagen = 'Imagen4.png';
+                
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+            cardBody.appendChild(uLista);
+            cardCliente.appendChild(card);
+            uLista.appendChild(liNombre);
+            uLista.appendChild(liFono);
+            uLista.appendChild(liMail);
+            uLista.appendChild(liMensaje);
+            uLista.appendChild(liImagen);
+            
 
-                uLista.appendChild(liNombre);
-                uLista.appendChild(liFono);
-                uLista.appendChild(liMail);
-                uLista.appendChild(liMensaje);
-                uLista.appendChild(liImagen);
-                listaServicios.appendChild(uLista);
-
-        }  if(elementos.opcion === "3"){
+        }if(elementos.opcion === "2"){
+            //creo el card
+            let card = document.createElement("div");
+            card.innerHTML = "";
+            card.classList.add("card","mt-5");
+            //creo el card header 
+            let cardHeader = document.createElement("div");
+            cardHeader.classList.add("card-header","bg-dark","text-white","text-center");
+            cardHeader.innerText = "Bienvenido a Ruta Verde " + elementos.nombre;
+            //creo el card body
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
             let uLista = document.createElement('ul');
             uLista.classList.add("list-group", "list-group-flush")
             uLista.innerHTML = "";
-             //creacion del Li para el nombre
+            let liNombre = document.createElement("li");
+            liNombre.classList.add("list-group-item");
+            liNombre.innerText = elementos.nombre + " usted escogio la actividad Caminitas Familiares";
+            //creacion del Li para el telefono
+            let liFono = document.createElement("li");
+            liFono.classList.add("list-group-item");
+            liFono.innerText = "Su telefono es : " + elementos.telefono
+            //creacion del Li para el correo
+            let liMail = document.createElement("li");
+            liMail.classList.add("list-group-item");
+            liMail.innerText = "Corre electonico : " + elementos.email + ", enviaremos mas informacion sobre la actividad.";
+            //creacion del Li para el mensaje
+            let liMensaje = document.createElement("li");
+            liMensaje.classList.add("list-group-item");
+            liMensaje.innerText = "Mensaje :" + elementos.mensaje;
+            //creacion del Li para la foto
+            liImagen = document.createElement("li");
+            liImagen.classList.add("list-group-item","p-2");
+            imagen = 'Imagen3.png';
+                
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+            cardBody.appendChild(uLista);
+            cardCliente.appendChild(card);
+            uLista.appendChild(liNombre);
+            uLista.appendChild(liFono);
+            uLista.appendChild(liMail);
+            uLista.appendChild(liMensaje);
+            uLista.appendChild(liImagen);
+
+        }  if(elementos.opcion === "3"){
+            //creo el card
+            let card = document.createElement("div");
+            card.innerHTML = "";
+            card.classList.add("card","mt-5");
+            //creo el card header 
+            let cardHeader = document.createElement("div");
+            cardHeader.classList.add("card-header","bg-dark","text-white","text-center");
+            cardHeader.innerText = "Bienvenido a Ruta Verde " + elementos.nombre;
+            //creo el card body
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+            let uLista = document.createElement('ul');
+            uLista.classList.add("list-group", "list-group-flush")
+            uLista.innerHTML = "";
             let liNombre = document.createElement("li");
             liNombre.classList.add("list-group-item");
             liNombre.innerText = elementos.nombre + " usted escogio la actividad Team Building";
@@ -120,24 +162,37 @@ window.cargarIngreso = ()=>{
             liMensaje.innerText = "Mensaje :" + elementos.mensaje;
             //creacion del Li para la foto
             liImagen = document.createElement("li");
-            liImagen.classList.add("list-group-item","p-5");
-            imagen = 'TeamBuilding.png';
-
-                uLista.appendChild(liNombre);
-                uLista.appendChild(liFono);
-                uLista.appendChild(liMail);
-                uLista.appendChild(liMensaje);
-                uLista.appendChild(liImagen);
-                listaServicios.appendChild(uLista);
+            liImagen.classList.add("list-group-item","p-2");
+            imagen = 'Imagen2.png';
+                
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+            cardBody.appendChild(uLista);
+            cardCliente.appendChild(card);
+            uLista.appendChild(liNombre);
+            uLista.appendChild(liFono);
+            uLista.appendChild(liMail);
+            uLista.appendChild(liMensaje);
+            uLista.appendChild(liImagen);
 
         }if(elementos.opcion === "4"){
+            //creo el card
+            let card = document.createElement("div");
+            card.innerHTML = "";
+            card.classList.add("card","mt-5");
+            //creo el card header 
+            let cardHeader = document.createElement("div");
+            cardHeader.classList.add("card-header","bg-dark","text-white","text-center");
+            cardHeader.innerText = "Bienvenido a Ruta Verde " + elementos.nombre;
+            //creo el card body
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
             let uLista = document.createElement('ul');
             uLista.classList.add("list-group", "list-group-flush")
             uLista.innerHTML = "";
-             //creacion del Li para el nombre
             let liNombre = document.createElement("li");
             liNombre.classList.add("list-group-item");
-            liNombre.innerText = elementos.nombre + " usted escogio la actividad Karaoke y Fogata";
+            liNombre.innerText = elementos.nombre + " usted escogio la actividad Fogatas y Karaoke";
             //creacion del Li para el telefono
             let liFono = document.createElement("li");
             liFono.classList.add("list-group-item");
@@ -149,18 +204,21 @@ window.cargarIngreso = ()=>{
             //creacion del Li para el mensaje
             let liMensaje = document.createElement("li");
             liMensaje.classList.add("list-group-item");
-            liMensaje.innerText = "Mensaje :" + elementos.mensaje;
+            liMensaje.innerHTML = "Mensaje :" + elementos.mensaje;
             //creacion del Li para la foto
             liImagen = document.createElement("li");
-            liImagen.classList.add("list-group-item","p-5");
-            imagen = 'imagen1.png';
-
-                uLista.appendChild(liNombre);
-                uLista.appendChild(liFono);
-                uLista.appendChild(liMail);
-                uLista.appendChild(liMensaje);
-                uLista.appendChild(liImagen);
-                listaServicios.appendChild(uLista);
+            liImagen.classList.add("list-group-item","p-2");
+            imagen = 'Imagen1.png';
+                
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+            cardBody.appendChild(uLista);
+            cardCliente.appendChild(card);
+            uLista.appendChild(liNombre);
+            uLista.appendChild(liFono);
+            uLista.appendChild(liMail);
+            uLista.appendChild(liMensaje);
+            uLista.appendChild(liImagen);
         }
 
         let img = document.createElement("img");
@@ -189,7 +247,7 @@ btnServicios.addEventListener('click', ()=>{
     let name = document.querySelector("#inputNombre").value.trim();
     let movile = document.querySelector("#inputTelefono").value.trim();
     let correo = document.querySelector("#inputEmail").value.trim();
-    let message = tinymce.get('txt-areaServicio').getContent();
+    let message = tinymce.get("txt-areaServicio").getContent();
     let selection = document.querySelector("#selectServicio").value;
 
     erroresLista = [];//creo lista para guardar los errores
